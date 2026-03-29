cff9236a20b449e17f9126be10ed6aad8fc9778e
Antoniuk Kirill <kyryloantoniuk@gmail.com>
2026-03-29 17:51:28 +0300

    234

--- src/pages/Hero/Hero.jsx
+++ src/pages/Hero/Hero.jsx
@@ -1,62 +1,27 @@
 import "./Hero.scss";
 import { useTranslation } from "react-i18next";
+import LogoSvg from "../../assets/Group 12.svg";
 
 export default function Hero() {
   const { t } = useTranslation();
   return (
     <>
       <section className="hero">
-        <h1 className="hero__title">{t("hero.title")}</h1>
-        <div className="hero__description">
-          <p className="hero__text">{t("hero.subtitle1")}</p>
-          <p className="hero__text">{t("hero.subtitle2")}</p>
-        </div>
-        <button className="hero__btn">{t("hero.button")} &rarr;</button>
-      </section>
-
-      <section className="description-section">
-        <div className="description-section__title-wrapper">
-          <h2 className="description-section__title">
-            <span className="description-section__line-one">
-              <span className="description-section__highlight">WE Build</span>{" "}
-              The Websites
-            </span>
-            <span className="description-section__line-two">
-              <span className="description-section__highlight">
-                Your Business
-              </span>
-              Deserves.
-            </span>
-          </h2>
-        </div>
-        <button className="hero__btn">Let’s talk</button>
-        <div className="description-section__box-container">
-          <div className="description-section__box-item">
-            <h3>Concept & Consultation</h3>
+        <div className="hero_container">
+          <img src={LogoSvg} alt="logo" />
+          svg
+          <div className="hero_content">
+            <h1>Beyond web designe</h1>
             <p>
-              Let's align on your vision. We start with a detailed consultation
-              to define the aesthetic and functional requirements that will set
-              your brand apart from the competition.
+              We build digital foundations for your business.
+              <br />
+              Ready to bring your vision to life? Use our interactive calculator
+              to see how we can fit your budget and timeline.
             </p>
-            <button>&#8592;</button>
-          </div>
-          <div className="description-section__box-item">
-            <h3>Concept & Consultation</h3>
-            <p>
-              Let's align on your vision. We start with a detailed consultation
-              to define the aesthetic and functional requirements that will set
-              your brand apart from the competition.
-            </p>
-            <button>&rarr;</button>
-          </div>
-          <div className="description-section__box-item">
-            <h3>Concept & Consultation</h3>
-            <p>
-              Let's align on your vision. We start with a detailed consultation
-              to define the aesthetic and functional requirements that will set
-              your brand apart from the competition.
-            </p>
-            <button>&#8594;</button>
+            <div className="hero_btn">
+              <button className="btn_light">More about us</button>
+              <button className="btn_dark">Contact us</button>
+            </div>
           </div>
         </div>
       </section>
